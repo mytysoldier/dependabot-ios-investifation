dbeaffb521e497ed6a0b0240620b525520338d4d
mytysoldier <yoshi008t@gmail.com>
2025-04-24 13:15:39 +0900

    deps commit first

---
+++ dependabot-investigation/dependabot-investigation/dependabot_investigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  dependabot_investigationApp.swift
+//  dependabot-investigation
+//
+//  Created by 高松由樹 on 2025/04/24.
+//
+
+import SwiftUI
+
+@main
+struct dependabot_investigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
